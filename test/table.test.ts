e8c5ddfcb607029bee6623e696f682ac2194e814
Takeshi <tak@prismify.co>
2023-01-29 21:59:39 -0700

    Update tests

--- test/table.test.ts
+++ test/table.test.ts
@@ -1,125 +1,202 @@
 import Airtable from '../src/airtable'
-import axios from 'axios'
-// @ts-ignore
-import HttpAdapter from 'axios/lib/adapters/http'
-import { start } from './server/records.server'
-import createRequestBody from './fixtures/records.create'
-import updateRequestBody from './fixtures/records.update'
+import nock from 'nock'
+import { getRecordResponse } from './fixtures/responses'
+import { mockRecordsApi } from './fixtures/mocks'
+import { API_ENDPOINT_SYNC_ID, API_KEY, API_VERSION, BASE_ID, RECORD_ID, TABLE_NAME } from './fixtures/constants'
 
-axios.defaults.adapter = HttpAdapter
+const airtable = new Airtable({
+  apiKey: API_KEY,
+  // endpointUrl: 'http://localhost',
+})
+const base = airtable.base(BASE_ID)
 
-describe('Table', function () {
-  const BASE_ID = 'abc'
-  const TABLE_NAME = 'My Table'
-  const API_KEY = 'key123'
-  const airtable = new Airtable({
-    apiKey: API_KEY,
-    endpointUrl: 'http://localhost',
-  })
-  const base = airtable.base(BASE_ID)
+const table = base.table(TABLE_NAME)
 
-  const table = base.table(TABLE_NAME)
+describe('Base Data', () => {
+  describe('Records', () => {
 
-  beforeAll(() => {
-    start()
-  })
 
-  describe('records()', () => {
-    it('returns a list of records', async () => {
-      const { records } = await table.records()
-      expect(records.length).toBeGreaterThan(0)
-    })
+    describe('List records', () => {
+      beforeEach(() => {
+        mockRecordsApi(nock(`https://api.airtable.com`)
+          .matchHeader('Authorization', `Bearer ${API_KEY}`))
 
-    it('queries by field: ["Name, "Client"]', async () => {
-      const { records } = await table.records({ fields: ['Name', 'Client'] })
-      expect(records.length).toBeGreaterThan(0)
-    })
-
-    it('queries by maxRecords of 3', async () => {
-      const { records, offset } = await table.records({ maxRecords: 3 })
-      expect(records.length).toEqual(3)
-      expect(typeof offset).toEqual('string')
-    })
-
-    it('queries by pageSize of 1', async () => {
-      const { records, offset } = await table.records({ pageSize: 1 })
-      expect(records.length).toBeGreaterThan(0)
-      expect(typeof offset).toEqual('string')
-    })
-
-    it('queries by sort', async () => {
-      const { records } = await table.records({
-        sort: [{ field: 'Name', direction: 'desc' }],
       })
-      expect(records.length).toBeGreaterThan(0)
+      afterEach(() => nock.cleanAll())
+      test('records()', async () => {
+        const result = await table.records()
+        expect(result.records).toHaveLength(2)
+      })
+      test('list()', async () => {
+        for await (const record of table.list()) {
+          expect(record).not.toHaveLength(0)
+        }
+      })
     })
 
-    it.todo('queries by view')
+    describe('Get record', () => {
+      beforeEach(() => {
+        mockRecordsApi(nock(`https://api.airtable.com`)
+          .matchHeader('Authorization', `Bearer ${API_KEY}`))
 
-    it.todo('queries by cellFormat')
-  })
-
-  describe('list()', () => {
-    it('auto-paginates through the list of records', async () => {
-      let count = 0
-      for await (const records of table.list({ pageSize: 1 })) {
-        expect(records.length).toBeGreaterThan(0)
-        count++
-      }
-      expect(count).toBeGreaterThan(0)
+      })
+      afterEach(() => nock.cleanAll())
+      test('findRecord()', async () => {
+        const result = await table.findRecord(RECORD_ID)
+        expect(result).toEqual(getRecordResponse)
+      })
     })
-  })
 
-  describe('findRecord()', () => {
-    it('retrieves a record', async () => {
-      const recordId = 'recABcLKRaQWszKp'
-      const record = await table.findRecord(recordId)
-      expect(record.id).toEqual(recordId)
+    describe('Update multiple records', () => {
+      beforeAll(() => {
+        mockRecordsApi(nock(`https://api.airtable.com`)
+          .matchHeader('Authorization', `Bearer ${API_KEY}`))
+
+      })
+      afterEach(() => nock.cleanAll())
+      test('updateRecords()', async () => {
+        const result = await table.updateRecords([{
+          "fields": {
+            "Address": "501 Twin Peaks Blvd",
+            "Name": "Twin Peaks",
+            "Visited": true
+          },
+          "id": "rec560UJdUtocSouk"
+        },
+        {
+          "fields": {
+            "Visited": true
+          },
+          "id": "rec3lbPRG4aVqkeOQ"
+        }])
+
+        expect(result.records).toHaveLength(2)
+      })
+
     })
-  })
 
-  describe('createRecord', () => {
-    it('creates a single record', async () => {
-      const { records } = await table.createRecord(createRequestBody[0])
-      expect(records).toEqual([createRequestBody[0]])
+    describe('Update Multiple Records - Perform Upsert', () => {
+      beforeAll(() => nock(`https://api.airtable.com`)
+        .matchHeader('Authorization', `Bearer ${API_KEY}`)
+        .patch(`/${API_VERSION}/${BASE_ID}/${TABLE_NAME}`)
+        .reply(200, {
+          "createdRecords": [
+            "recsHMqsp3GEm3lEi"
+          ],
+          "records": [
+            {
+              "createdTime": "2022-09-12T21:03:48.000Z",
+              "fields": {
+                "Address": "501 Twin Peaks Blvd",
+                "Name": "Twin Peaks",
+                "Visited": true
+              },
+              "id": "rec560UJdUtocSouk"
+            },
+            {
+              "createdTime": "2022-11-15T01:02:04.400Z",
+              "fields": {
+                "Name": "New Park",
+                "Visited": true
+              },
+              "id": "recsHMqsp3GEm3lEi"
+            }
+          ],
+          "updatedRecords": [
+            "rec560UJdUtocSouk"
+          ]
+        })
+      )
+      afterEach(() => nock.cleanAll())
+
+      test('updateRecordsUsingUpsert()', async () => {
+        const result = await table.updateRecordsUsingUpsert(['Names'], [
+          {
+            "fields": {
+              "Address": "501 Twin Peaks Blvd",
+              "Name": "Twin Peaks",
+              "Visited": true
+            }
+          },
+          {
+            "fields": {
+              "Name": "New Park",
+              "Visited": true
+            }
+          }
+        ])
+
+        expect(result).toHaveProperty('createdRecords');
+        expect(result).toHaveProperty('updatedRecords');
+        expect(result.createdRecords).toHaveLength(1);
+        expect(result.updatedRecords).toHaveLength(1);
+      })
     })
-  })
 
-  describe('createRecords', () => {
-    it('creates a batch of records', async () => {
-      const { records } = await table.createRecords(createRequestBody)
-      expect(records).toEqual(createRequestBody)
+    describe('Create records', () => {
+      beforeAll(() => mockRecordsApi(nock(`https://api.airtable.com`)))
+      afterAll(() => nock.cleanAll())
+
+      test('createRecords()', async () => {
+        const result = await table.createRecords([{
+          "fields": {
+            "Address": "501 Twin Peaks Blvd",
+            "Name": "Twin Peaks",
+            "Visited": true
+          }
+        },
+        {
+          "fields": {
+            "Name": "New Park",
+            "Visited": true
+          }
+        }])
+
+        expect(result.records).toHaveLength(2)
+      })
+
+      test('createRecord()', async () => {
+        const result = await table.createRecord({
+          "fields": {
+            "Address": "501 Twin Peaks Blvd",
+            "Name": "Twin Peaks",
+            "Visited": true
+          }
+        })
+
+        expect(result).toHaveProperty('id')
+      })
     })
-  })
 
-  describe('updateRecord', () => {
-    it('updates a single record', async () => {
-      const { records } = await table.updateRecord(updateRequestBody[0])
-      expect(records).toEqual([updateRequestBody[0]])
+    describe('Delete multiple records', () => {
+      beforeAll(() => mockRecordsApi(nock(`https://api.airtable.com`)))
+      afterAll(() => nock.cleanAll())
+
+      test('deleteRecords()', async () => {
+        const result = await table.deleteRecords([RECORD_ID])
+        expect(result.records).toHaveLength(2)
+      })
     })
-  })
 
-  describe('updateRecords', () => {
-    it('updates a batch of records', async () => {
-      const { records } = await table.updateRecords(updateRequestBody)
-      expect(records).toEqual(updateRequestBody)
+    describe('Delete record', () => {
+      beforeAll(() => mockRecordsApi(nock(`https://api.airtable.com`)))
+      afterAll(() => nock.cleanAll())
+
+      test('deleteRecord()', async () => {
+        const result = await table.deleteRecord(RECORD_ID)
+        expect(result).toHaveProperty('id')
+      })
     })
-  })
 
-  describe('deleteRecords', () => {
-    it('deletes a single records', async () => {
-      const { records } = await table.deleteRecord('recABcLKRaQWszKp')
-      expect(records.length).toEqual(1)
-    })
-  })
+    describe('Sync CSV', () => {
+      beforeAll(() => mockRecordsApi(nock(`https://api.airtable.com`)))
+      afterAll(() => nock.cleanAll())
 
-  describe('deleteRecords', () => {
-    it('deletes a batch of records', async () => {
-      const { records } = await table.deleteRecords([
-        'recABcLKRaQWszKp',
-        'recAtELIMS1xJOTI',
-      ])
-      expect(records.length).toEqual(2)
+      test.todo('syncCsv()', async () => {
+        await expect(table.sync(API_ENDPOINT_SYNC_ID, `'column1,column2
+row1-column1,row1-column2
+row2-column1,row2-column2'`)).resolves.toBeCalled()
+      })
     })
   })
 })
